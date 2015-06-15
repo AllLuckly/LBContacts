63af461cdcd2e00e1dd4523054103be6a7b3bce5
AllLuckly <435844159@qq.com>
2015-06-15 17:59:41 +0800

    Initial Commit

---
+++ LBContacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBContacts
+//
+//  Created by fhkj on 15/6/15.
+//  Copyright (c) 2015年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
